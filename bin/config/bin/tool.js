5d6360050c536b2b7bf3ffe7f6adc904919aa759
Time Hu <timemy.hu@gmail.com>
2019-05-10 10:54:15 +0800

    1

--- bin/config/bin/tool.js
+++ bin/config/bin/tool.js
@@ -1,13 +1,16 @@
 /**
  *  Created by hu on 2019-04-26.
  **/
-const path = require('path');
+const Path = require('path');
 const Os = require('os');
-const fs = require('fs');
-const envPublicUrl = process.env.PUBLIC_URL;
-const appDirectory = fs.realpathSync(process.cwd());
+const Fs = require('fs');
+const Cp  = require('child_process');
+const Net = require('net');
 
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const envPublicUrl = process.env.PUBLIC_URL;
+const appDirectory = Fs.realpathSync(process.cwd());
+
+const resolveApp = relativePath => Path.resolve(appDirectory, relativePath);
 const getPublicUrl = appPackageJson =>envPublicUrl || require(appPackageJson).homepage;
 
 function ensureSlash(inputPath, needsSlash) {
@@ -27,16 +30,32 @@ function getServedPath(appPackageJson){
         envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
     return ensureSlash(servedUrl, true);
 }
-
+/**
+ * 获取package.json文件中某个key下面的value
+ * @method getPackageValue
+ * @param {string} key 需要获取的key
+ * @return {undefined|any} 如果没有该定义该key返回undefined，否则返回当前key的value
+ */
 function getPackageValue(key){
     const  Package=require(resolveApp('package.json'))
     return Package[key]!=null?Package[key]:undefined
 }
-
+/**
+ * 根据name与ext拼接完整的文件名称
+ * @method 名称
+ * @for JJTool
+ * @param {string} name 文件名
+ * @param {string} ext 文件类型
+ * @return {string} 完整的文件名
+ */
 function fsToExist(name,ext='js'){
     let pathexist=`${name}.${ext}`;
 }
-
+/**
+ * 获取本地可用的Ip
+ * @method getIP
+ * @return {Array} 返回可用本地IP
+ */
 function getIP(){
     const IPv4=[];
     const networkInterfaces=Os.networkInterfaces();
@@ -50,5 +69,24 @@ function getIP(){
     });
     return IPv4;
 }
-
-module.exports={resolveApp,getServedPath,getPackageValue,fsToExist,getIP}
+/**
+ * 根据输入的端口打开浏览器
+ * @method openBrowser
+ * @param {number} prot 描述
+ */
+function openBrowser(prot) {
+    switch (process.platform) {
+        //mac系统使用 一下命令打开url在浏览器
+        case "darwin":
+            Cp.exec(`open http://localhost:${prot}`);
+            break;
+        //win系统使用 一下命令打开url在浏览器
+        case "win32":
+            Cp.exec(`start http://localhost:${prot}`);
+            break;
+        // 默认mac系统
+        default:
+            Cp.exec(`open http://localhost:${prot}`);
+    }
+}
+module.exports={resolveApp,getServedPath,getPackageValue,fsToExist,getIP,openBrowser}
--- bin/config/server.js
+++ bin/config/server.js
@@ -1,7 +1,6 @@
 /**
  *  Created by hu on 2019-04-22.
  **/
-const Cp  = require('child_process');
 const Paths=require('./paths');
 const Tool = require('./bin/tool');
 
@@ -12,7 +11,7 @@ module.exports=(prot,pathSrc)=>{
         contentBase:pathSrc,
         historyApiFallback: true,
         hot:true,
-        hotOnly: true,
+        hotOnly: true, //布局刷新
         inline: true,
         progress: true,
         allowedHosts:allowedHosts,//添加白名单服务
@@ -20,7 +19,6 @@ module.exports=(prot,pathSrc)=>{
         port:prot,
         disableHostCheck: true,
         host:'0.0.0.0',
-        // open: 'Google Chrome',
         stats: Paths.appProxy.stats,
         proxy:{
             ...Paths.appProxy.data
@@ -30,20 +28,7 @@ module.exports=(prot,pathSrc)=>{
                 console.info(`\u001b[47;30m 服务已启动一下Url可以访问: \u001b[0m`);
                 console.info(`\u001b[47;30m ${index}: \u001b[42;30m "http://${item}:${prot}"  入口!!\u001b[0m`);
             })
-            switch (process.platform) {
-                //mac系统使用 一下命令打开url在浏览器
-                case "darwin":
-                    Cp.exec(`open http://localhost:${prot}`);
-                    break;
-                //win系统使用 一下命令打开url在浏览器
-                case "win32":
-                    Cp.exec(`start http://localhost:${prot}`);
-                    break;
-                // 默认mac系统
-                default:
-                    Cp.exec(`open http://localhost:${prot}`);
-            }
-
+            Tool.openBrowser(prot);
         }
     }
 }
